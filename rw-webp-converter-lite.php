acff60d29b9c97db176f2c1e8b95515c7e28ca9a
Robert South <robert2021south@gmail.com>
2025-12-08 11:52:55 +0800

    add rw-webp-convert-lite.php

---
+++ rw-webp-converter-lite.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: RW Image Optimizer Lite
+ * Description:
+ * Version: 1.0.2
+ * Author: RobertWP (Robert South)
+ * Author URI: https://robertwp.com
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: rw-image-optimizer-lite
+ * Domain Path: /languages
+ */
+
+namespace RobertWP\ImageOptimizerLite;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'RWWCL_PLUGIN_NAME', 'RW Image Optimizer Lite' );
+define( 'RWWCL_VERSION_OPTION', 'rwwcl_version' );
+define( 'RWWCL_PLUGIN_VERSION', '1.0.0' );
+define( 'RWWCL_PLUGIN_FILE', __FILE__ );
+define( 'RWWCL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'RWWCL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'RWWCL_ASSETS_URL', RWWCL_PLUGIN_URL . 'assets/' );
+
+require_once RWWCL_PLUGIN_DIR . 'includes/core/plugin.php';
+
